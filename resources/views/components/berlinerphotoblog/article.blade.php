352706a9b781fe28f45b803c50d9ea485466a81c
Jens Freudenau <jens@freude-now.de>
2025-05-20 16:36:23 +0200

    redesign blog pade

--- resources/views/components/berlinerphotoblog/article.blade.php
+++ resources/views/components/berlinerphotoblog/article.blade.php
@@ -1,9 +1,5 @@
 @php use Carbon\Carbon; use Illuminate\Support\Str; @endphp
 <article class=" mt-4 p-6 bg-white border border-gray-50 shadow-sm">
-    <div class="flex float-right justify-between items-center mb-5 text-gray-900">
-        <span class="text-sm">{{ Carbon::parse($content->date)->format('d.m.Y')}}</span>
-    </div>
-
     <div class="m-4">
         @php
             $media = $content->getFirstMedia('images');
@@ -21,9 +17,9 @@
         $mediaItemsAll->all()
     @endphp
 
-    <div class="justify-between items-stretch">
+    <div class=" ">
         <div class="container mx-auto px-5 py-2">
-            <div class="-m-1 flex flex-wrap ">
+            <div class="m-1 flex flex-wrap ">
                 @if($numberOfImages === 2)
                     <x-berlinerphotoblog.twoAdditionalImages :content="$content" :mediaItemsAll="$mediaItemsAll"></x-berlinerphotoblog.twoAdditionalImages>
                 @else
@@ -32,23 +28,27 @@
             </div>
             @if($single === 'true')
                 <div class="flex justify-start">
-                    <h2 class="text-5xl font-extrabold ml-3 pb-6">{{$content->header}}</h2>&nbsp;  &nbsp;
+                    <h2 class="text-2xl ml-3 pb-1">{{$content->header}}</h2>&nbsp;  &nbsp;
                    </div>
             @else
-                <h2 class="underline text-2xl   ml-3 pb-6 font-extrabold "><a href="/single/{{$content->slug}}" >{{$content->header}}</a></h2>
+                <h2 class="text-2xl ml-3 pb-1"><a href="/single/{{$content->slug}}" >{{$content->header}}</a></h2>
             @endif
-            @if(!empty($content->text))
-                <p class="mb-4 font-thin text-base"> {!! Str::markdown($content->text ) !!} </p>
-            @endif
-            <h3 class="underline text-xl tracking-tight mt-3 pb-6 ">
-                <a href="/getCategory/{{$content->category?->name}}" class="underline">{{$content->category?->name}}</a>
-            </h3>
         </div>
-        <div class="px-4">
-            @foreach($tags as $key => $tag)
-                <x-button-tag class="dark:bg-sky-100 bg-sky-100" href="/tag/{{$tag}}">#{{$tag}}</x-button-tag>
-            @endforeach
+        <div class=" ">
+            <div class="  ml-10 float-left   mb-5 ">
+                <h3><span class="text-sm text-gray-300">{{ Carbon::parse($content->date)->format('d.m.Y')}}</span>
+                <a href="/getCategory/{{$content->category?->name}}" class="font-thin text-gray-500 ml-2">{{$content->category?->name}}</a></h3>
+                @if(!empty($content->text))
+                    <p class="mb-4 font-thin text-base text-gray-900"> {!! Str::markdown($content->text ) !!} </p>
+                @endif
+            </div>
 
         </div>
+
+    </div>
+    <div class="p-3 pl-7 pt-16 text-gray-300">
+        @foreach($tags as $key => $tag)
+            <x-button-tag class="dark:bg-sky-100 p-0 m-0 bg-white" href="/tag/{{$tag}}">#{{$tag}}</x-button-tag>
+        @endforeach
     </div>
 </article>
